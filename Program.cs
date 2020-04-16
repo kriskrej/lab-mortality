510a56bfa6ec127170d8de083477478a6bd0a7cb
Kris Krej <kris@krej.net>
2020-04-16 10:10:53 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Linq;
 
 namespace MortalityWHO {
     class Program {
         static void Main() {
             var mortalityDb = new MortalityDb();
+            foreach (var reason in mortalityDb.records.OrderBy(r=>r.deaths1))
+                Console.WriteLine(reason.cause + " - " + reason.deaths1);
         }
     }
 }
